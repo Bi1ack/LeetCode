eeb3a97ac99cf517d0e63a001119e4579816ee08
zr <804654763@qq.com>
2021-10-20 19:54:19 +0800

    453. Minimum Moves to Equal Array Elements

---
+++ src/Perday/MinimumMovestoEqualArrayElements.java
@@ -0,0 +1,16 @@
+package Perday;
+
+
+import java.util.Arrays;
+
+public class MinimumMovestoEqualArrayElements {
+    public int minMoves(int[] nums) {
+        int minNum = Arrays.stream(nums).min().getAsInt();
+        int res = 0;
+        for (int num : nums) {
+            res += num - minNum;
+        }
+        return res;
+    }
+
+}
