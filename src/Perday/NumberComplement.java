0a89f2e7f0f8937129eb258b257901b95c61b194
zr <804654763@qq.com>
2021-10-22 11:00:45 +0800

    Sorting Algorithm

---
+++ src/Perday/NumberComplement.java
@@ -0,0 +1,21 @@
+package Perday;
+
+
+public class NumberComplement {
+    /**
+     * @author zr
+     * @date 2021.10.18
+     * @return
+     */
+
+    public static int findComplement(int num) {
+        // Todo
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int a = 15;
+        int res = findComplement(a);
+        System.out.print(res);
+    }
+}
