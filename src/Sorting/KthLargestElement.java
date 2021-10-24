d443f2902e8af0c256fb8b4b22bd061a1a9fbaea
zr <804654763@qq.com>
2021-10-24 23:30:38 +0800

    min heap

--- src/Sorting/KthLargestElement.java
+++ src/Sorting/KthLargestElement.java
@@ -1,5 +1,7 @@
 package Sorting;
 
+import java.util.Random;
+
 /**
  * @author zr
  * @date 2021.10.24
@@ -7,14 +9,72 @@ package Sorting;
 public class KthLargestElement {
     /**
      * @param arr : a sorted array
-     * @param k : k-th largest num
+     * @param k   : k-th largest num
      */
     public static int findKthLargest(int[] arr, int k) {
+        //todo
         int res = 0;
         return res;
     }
 
-    public static void main(String[] args) {
+    public static void heapSort(int[] arr) {
+        //建堆
+        for (int i = 0; i < arr.length; i++) {
+            heapInsert(arr, i);
+        }
+        //堆化
+        int heapSize = arr.length;
+        swap(arr, 0, --heapSize);
+        while (heapSize > 0) {
+            heapify(arr, 0, heapSize);
+            swap(arr, 0, --heapSize);
+        }
+    }
 
+    public static void heapInsert(int[] arr, int index) {
+        int par = (index - 1) / 2;
+        while (par >= 0 && arr[index] < arr[par]) {
+            swap(arr, index, par);
+            index = par;
+            par = (index - 1) / 2;
+        }
+    }
+
+    public static void heapify(int[] arr, int index, int heapSize) {
+        int left = 2 * index + 1;
+        while (left < heapSize) {
+            int min = left + 1 < heapSize && arr[left] > arr[left + 1] ? left + 1 : left;
+            min = arr[min] < arr[index] ? min : index;
+            if (min == index) {
+                break;
+            } else {
+                swap(arr, index, min);
+                index = min;
+                left = 2 * index + 1;
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            arr[i] = random.nextInt(100);
+        }
+        for (int i : arr) System.out.print(i + " ");
+        System.out.println();
+        int x = random.nextInt(9);
+        int a = 0, b = 0;
+//        a = arr[x] > arr[x + 1] ? x + 1 : x;
+//        b = arr[x] < arr[x + 1] ? x : x + 1;
+//        System.out.print(a + " " + b);
+        heapSort(arr);
+        for (int i : arr) System.out.print(i + " ");
     }
 }
