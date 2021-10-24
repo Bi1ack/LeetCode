df289cd68192c012744b19bf1a0ef3b569cf2396
zr <804654763@qq.com>
2021-10-24 18:09:59 +0800

    update

---
+++ src/Sorting/KthLargestElement.java
@@ -0,0 +1,20 @@
+package Sorting;
+
+/**
+ * @author zr
+ * @date 2021.10.24
+ */
+public class KthLargestElement {
+    /**
+     * @param arr : a sorted array
+     * @param k : k-th largest num
+     */
+    public static int findKthLargest(int[] arr, int k) {
+        int res = 0;
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
