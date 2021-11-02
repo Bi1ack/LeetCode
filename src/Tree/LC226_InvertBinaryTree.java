d017a7b174cc9e0bab101e481a2409fc79b891d6
zr <804654763@qq.com>
2021-11-02 22:43:39 +0800

    226. Invert Binary Tree

---
+++ src/Tree/LC226_InvertBinaryTree.java
@@ -0,0 +1,25 @@
+package Tree;
+
+/**
+ * @program: LeetCode
+ * @description: Given the root of a binary tree, invert the tree, and return its root.
+ * @author: zr
+ * @date: 2021-11-02 22:21
+ **/
+public class LC226_InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        invertTree(root.left);
+        invertTree(root.right);
+        if (root.left != null || root.right != null) {
+            swap(root);
+        }
+        return root;
+    }
+
+    public void swap(TreeNode node) {
+        TreeNode tmp = node.left;
+        node.left = node.right;
+        node.right = tmp;
+    }
+}
