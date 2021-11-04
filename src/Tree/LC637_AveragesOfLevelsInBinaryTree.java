d1656230a6241a01533ed2960c9bbc334d63de0f
zr <804654763@qq.com>
2021-11-04 21:54:25 +0800

    first commit

---
+++ src/Tree/LC637_AveragesOfLevelsInBinaryTree.java
@@ -0,0 +1,39 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LC637_AveragesOfLevelsInBinaryTree {
+    public List<Double> averagesOfLevels(TreeNode root) {
+        List<Double> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        TreeNode curEnd = root;
+        int sum = 0, count = 0;
+        TreeNode nextEnd = null;
+        while(!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.left != null) {
+                queue.offer(node.left);
+                nextEnd = node.left;
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+                nextEnd = node.right;
+            }
+            sum += node.val;
+            ++count;
+            if(node == curEnd) {
+                res.add((double) (sum / count));
+                sum = 0;
+                count = 0;
+                curEnd = nextEnd;
+                nextEnd = null;
+            }
+
+        }
+        return res;
+    }
+}
