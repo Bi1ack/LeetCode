259c392d5ee05e1f0d000f97598ca124a7a75126
zr <804654763@qq.com>
2021-11-08 13:49:57 +0800

    First commit

---
+++ src/Perday/LC1218_LongestArithmeticSubsequenceOfGivenDifference.java
@@ -0,0 +1,14 @@
+package Perday;
+
+/**
+ * @program: LeetCode
+ * @description: lc 1218
+ * @author: zr
+ * @date: 2021-11-07 12:00
+ **/
+public class LC1218_LongestArithmeticSubsequenceOfGivenDifference {
+    public int longestSubsequence(int[] arr, int difference) {
+        // TODO: 2021/11/7
+        return -1;
+    }
+}
---
+++ src/Tree/LC337_HouseRobber3.java
@@ -0,0 +1,24 @@
+package Tree;
+
+import DataStructure.TreeNode;
+import com.sun.source.tree.Tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @program: LeetCode
+ * @description: 不相邻节点最大和
+ * @author: zr
+ * @date: 2021-11-07 15:40
+ **/
+public class LC337_HouseRobber3 {
+    public int rob(TreeNode root) {
+        return -1;
+    }
+
+    public int rob_rec(TreeNode root) {
+        // TODO: 2021/11/7
+        return -1;
+    }
+}
