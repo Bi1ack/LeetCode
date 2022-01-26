315d7a908d971ffe232c4e2ea540361d4fecdc80
zr <804654763@qq.com>
2022-01-26 16:40:10 +0800

    first commit

---
+++ src/DP/LC413_numberOfArithmeticSlices.java
@@ -0,0 +1,48 @@
+package DP;
+
+/**
+ * @program: LeetCode
+ * @description:
+ * @author: zr
+ * @date: 2022-01-26 16:03
+ **/
+public class LC413_numberOfArithmeticSlices {
+    public int numberOfArithmeticSlices(int[] A) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int n = A.length;
+        int[] dp = new int[n];
+        for (int i = 2; i < n; i++) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                dp[i] = dp[i - 1] + 1;
+            }
+        }
+        int total = 0;
+        for (int cnt : dp) {
+            total += cnt;
+        }
+        return total;
+    }
+
+    public int numberOfArithmeticSlices_(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return 0;
+        }
+
+        int d = nums[0] - nums[1], t = 0;
+        int ans = 0;
+        // 因为等差数列的长度至少为 3，所以可以从 i=2 开始枚举
+        for (int i = 2; i < n; ++i) {
+            if (nums[i - 1] - nums[i] == d) {
+                ++t;
+            } else {
+                d = nums[i - 1] - nums[i];
+                t = 0;
+            }
+            ans += t;
+        }
+        return ans;
+    }
+}
