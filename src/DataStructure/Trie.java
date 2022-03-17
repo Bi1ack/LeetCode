6edf9965206a7b641301d6a83bb5dc1f54665930
zr <804654763@qq.com>
2022-03-17 10:11:54 +0800

    modified code

--- src/DataStructure/Trie.java
+++ src/DataStructure/Trie.java
@@ -8,12 +8,44 @@ package DataStructure;
  **/
 public class Trie {
 
-    public int val;
-    public Trie[] next;
-
-    public Trie () {
-        val = 0;
-        next = new Trie[26];
+    public class Node {
+        Node[] next = new Node[26];
+        boolean isLeaf;
     }
 
+    private Node root = new Node();
+    private Node node = root;
+
+    public Trie () {
+
+    }
+
+    public void insert (String word) {
+        node = root;
+        for (char c : word.toCharArray()) {
+            if (node.next[c - 'a'] == null) {
+                node.next[c - 'a'] = new Node();
+            }
+            node = node.next[c - 'a'];
+        }
+        node.isLeaf = true;
+    }
+
+    public boolean search (String word) {
+        node = root;
+        for (char c : word.toCharArray()) {
+            if (node.next[c - 'a'] == null) return false;
+            node = node.next[c - 'a'];
+        }
+        return node.isLeaf;
+    }
+
+    public boolean startsWith (String prefix) {
+        node = root;
+        for (char c : prefix.toCharArray()) {
+            if (node.next[c - 'a'] == null) return false;
+            node = node.next[c - 'a'];
+        }
+        return true;
+    }
 }
