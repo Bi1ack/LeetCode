c2104cb9c8ccbc29e8b843e9aadcd50e1af6aa19
zr <804654763@qq.com>
2022-04-10 11:47:45 +0800

    first commit

---
+++ src/CodeInterview/Code49_nthUglyNumber.java
@@ -0,0 +1,30 @@
+package CodeInterview;
+
+/**
+ * @program: LeetCode
+ * @description:
+ * @author: Ray
+ * @date: 2022-04-09 09:48
+ **/
+public class Code49_nthUglyNumber {
+
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        int p2 = 1, p3 = 1, p5 = 1;
+        for (int i = 2; i <= n; i++) {
+            int num2 = dp[p2] * 2, num3 = dp[p3] * 3, num5 = dp[p5] * 5;
+            dp[i] = Math.min(Math.min(num2, num3), num5);
+            if (dp[i] == num2) {
+                p2++;
+            }
+            if (dp[i] == num3) {
+                p3++;
+            }
+            if (dp[i] == num5) {
+                p5++;
+            }
+        }
+        return dp[n];
+    }
+}
